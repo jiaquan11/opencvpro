36a867f454f2d709c252842df74975d138e1261b
jiaquan11 <913882743@qq.com>
2025-05-07 16:13:26 +0800

    opencv480_quick_tutorial项目：搭建环境，用于opencv简单用例的快速学习

---
+++ opencv480_quick_tutorial/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv) {
+	Mat src = imread("lena.png", IMREAD_GRAYSCALE);
+	if (src.empty()) {
+		printf("could not load image...\n");
+		return -1;
+	}
+	namedWindow("输入窗口", WINDOW_FREERATIO);
+	imshow("输入窗口", src);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
